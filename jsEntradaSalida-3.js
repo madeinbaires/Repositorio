2ff84a9630bab46b0e7dbaccb29f6b9236a90fda
alumno <alumno@utnfra.com.ar>
2020-01-08 16:04:12 -0300

    ejercicios clase 2

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+var dato;
+dato = document.getElementById("elNombre").value;//en el Ej 2 hacia lo mismo pero con prompt.
+
+alert(dato);
+
+}
+
+
