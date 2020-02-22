463a3501110eff0fc7c3caea572932ce8c1ce4d7
madeinbaires <52429399+madeinbaires@users.noreply.github.com>
2020-02-22 11:28:35 -0300

    Add files via upload

--- jsIteraciones (1).js
+++ jsIteraciones (1).js
@@ -1,14 +1,12 @@
 function mostrar()
 {
-	var contador=0;
-									 
-							
-	while (contador <10){
-        contador++
-		console.log (contador);
-	}
+	var contador = 0;
+     
+	  while (contador < 10)
+{
+	contador++;
+	document.write(contador+ "<br>") ;
 	
 
 }
-
-//al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10.
+}
--- jsIteraciones (6).js
+++ jsIteraciones (6).js
@@ -5,19 +5,23 @@ function mostrar()
 	var acumulador=0;
 	var numero;
 
-	
+//Como no tengo un espacio para ingresar el numero,lo pido por prompt.
 
-	while (contador<5){
-		numero=prompt("Ingrese un numero");
-		numero=parseInt(numero);
-		acumulador+=numero;
-		contador++; 
-	} 
+while(contador < 5){   	
 
-	
+numero = prompt ("Ingrese un numero");
+ numero=parseInt(numero);
+ acumulador = acumulador + numero;
+ contador++
+
+}
+//Los promedios y los porcentajes se calculan afuera del while.
+
+
+
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/contador;
 
-}
+}//FIN DE LA FUNCIÓN
